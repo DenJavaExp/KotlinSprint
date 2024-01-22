b97d0a7be4f2cd4b2249f3800f3776c456fde9bc
Denis Marmyshev <marmyshev.denis@mail.ru>
2024-01-22 19:58:16 +0300

    KS 1_2. Скопировал решение задачи №1, добавил новую переменную с количеством работников и добавил увольнение 1 работника.

---
+++ src/main/kotlin/lesson1_task2.kt
@@ -0,0 +1,13 @@
+fun main() {
+
+    var numberOfOrder: Int = 75
+    val gratitude: String = "Thanks for the purchase"
+    var numberOfEmployees: Int = 2000
+
+    numberOfEmployees -= 1
+
+    println(numberOfOrder)
+    println(gratitude)
+    println(numberOfEmployees)
+
+}
