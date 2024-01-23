9403a18827a66b84f849e797b8aaa521f6ee65a3
Denis Marmyshev <marmyshev.denis@mail.ru>
2024-01-23 11:33:29 +0300

    KS 1_5. Закоментировал метод вывода кол-во сотрудников  присвоил перемнной numberOfEmployees новое значение.

--- src/main/kotlin/lesson1_task2.kt
+++ src/main/kotlin/lesson1_task2.kt
@@ -3,11 +3,8 @@ fun main() {
     var numberOfOrder: Int = 75
     val gratitude: String = "Thanks for the purchase"
     var numberOfEmployees: Int = 2000
-
-    numberOfEmployees -= 1
-
     println(numberOfOrder)
     println(gratitude)
-    println(numberOfEmployees)
-
+    //println(numberOfEmployees)
+    numberOfEmployees = 1999
 }
