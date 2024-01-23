2b356b968b555147849801ebdee2642cfbb71fea
Denis Marmyshev <marmyshev.denis@mail.ru>
2024-01-23 12:05:03 +0300

    KS 1_5. Создал константу NUMBER.

--- src/main/kotlin/lesson1_task5.kt
+++ src/main/kotlin/lesson1_task5.kt
@@ -1,10 +1,10 @@
 fun main() {
+    val NUMBER = 60
     val secondsAll = 6480
-    val minutesAll = secondsAll / 60
-    val sec = secondsAll % 60
-    val min = minutesAll % 60
-    val hours = minutesAll / 60
-
+    val minutesAll = secondsAll / NUMBER
+    val sec = secondsAll % NUMBER
+    val min = minutesAll % NUMBER
+    val hours = minutesAll / NUMBER
     val time = String.format("%02d:%02d:%02d", hours, min, sec)
     println(time)
 }
