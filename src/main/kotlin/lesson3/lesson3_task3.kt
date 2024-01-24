3563cecf55c5dc57f4ffd940a27e3461a9c93c59
Denis Marmyshev <marmyshev.denis@mail.ru>
2024-01-24 16:18:12 +0300

    KS 3_3. Вывел вычисления в строке используя интерполяцию.

---
+++ src/main/kotlin/lesson3/lesson3_task3.kt
@@ -0,0 +1,20 @@
+package lesson3
+
+fun main() {
+
+    var a = 7
+    var tabl = """
+      $a * 1 = ${a * 1} 
+      $a * 2 = ${a * 2} 
+      $a * 3 = ${a * 3} 
+      $a * 4 = ${a * 4} 
+      $a * 5 = ${a * 5} 
+      $a * 6 = ${a * 6} 
+      $a * 7 = ${a * 7} 
+      $a * 8 = ${a * 8} 
+      $a * 9 = ${a * 9}  
+     """.trimIndent()
+
+    println(tabl)
+}
+
