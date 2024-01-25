d37fff096dd154ad5a16adacc6445e6a0b99087f
Denis Marmyshev <marmyshev.denis@mail.ru>
2024-01-25 21:22:26 +0300

    KS 3_4. Разделил переменные от функций пустой срокой, добавил ход в переменную strokeNumber.

--- src/main/kotlin/lesson3/lesson3_task4.kt
+++ src/main/kotlin/lesson3/lesson3_task4.kt
@@ -5,9 +5,11 @@ fun main() {
     var fromWhich = "E2"
     var where = "E4"
     var strokeNumber = 1
+
     println("$fromWhich - $where; $strokeNumber")
     fromWhich = "D2"
     where = "D3"
+    strokeNumber = 2
     println("$fromWhich - $where; $strokeNumber")
 
 }
