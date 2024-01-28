a9f781e728393a8ce37d44a99d678e0e58ae661a
Denis Marmyshev <marmyshev.denis@mail.ru>
2024-01-28 21:21:30 +0300

    KS 4_1. Изменил оператор сравнения.

--- src/main/kotlin/lesson4/lesson4_task1.kt
+++ src/main/kotlin/lesson4/lesson4_task1.kt
@@ -2,12 +2,12 @@ package lesson4
 
 fun main() {
 
-    var reservedBookedToday = 13
-    var reservedBookedTomorrow = 4
+    val reservedBookedToday = 13
+    val reservedBookedTomorrow = 4
 
     println(
-        "Доступность столиков на сегодня: ${reservedBookedToday != NUMBER_OF_TABLES} " +
-                "\nДоступность столиков на завтра: ${reservedBookedTomorrow != NUMBER_OF_TABLES}"
+        "Доступность столиков на сегодня: ${reservedBookedToday < NUMBER_OF_TABLES } " +
+                "\nДоступность столиков на завтра: ${reservedBookedTomorrow < NUMBER_OF_TABLES}"
     )
 
 }
