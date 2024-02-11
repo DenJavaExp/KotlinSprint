673e7f256a44dca7374bbf7f4929feb6c91f0b94
Denis Marmyshev <marmyshev.denis@mail.ru>
2024-02-11 19:59:14 +0200

    KS 8_1. Задача 1 к Уроку 8
            Скрипт считает количество просмотров рекламы за день. Напиши программу, которая сохраняет в массив данные просмотров за неделю (числа использовать на свое усмотрение). Каждая ячейка хранит количество просмотров за день. Имея массив с данными, нужно посчитать сколько всего было просмотров за неделю. Результат вычислений выведи в консоль.

---
+++ src/main/kotlin/lesson8/lesson8_task1.kt
@@ -0,0 +1,14 @@
+package lesson8
+
+fun main() {
+
+    val views = arrayOf(4, 5, 7, 3, 0, 10, 1, 2)
+    var advertisement = 0
+    for (i in views) {
+        advertisement += i
+    }
+    println(advertisement)
+
+}
+
+
