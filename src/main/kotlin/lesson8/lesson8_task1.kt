18bcc3ece21fe0d5d798e91b098976c56757e866
Denis Marmyshev <marmyshev.denis@mail.ru>
2024-02-13 21:10:48 +0200

    KS 8_1. Задача 1 к Уроку 8
         Заменил цикл for на функцию sum().

--- src/main/kotlin/lesson8/lesson8_task1.kt
+++ src/main/kotlin/lesson8/lesson8_task1.kt
@@ -3,11 +3,9 @@ package lesson8
 fun main() {
 
     val views = arrayOf(4, 5, 7, 3, 0, 10, 1, 2)
-    var advertisement = 0
-    for (i in views) {
-        advertisement += i
-    }
+    val advertisement = views.sum()
     println(advertisement)
+
 }
 
 
