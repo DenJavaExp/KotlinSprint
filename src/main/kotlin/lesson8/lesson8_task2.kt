ae733e5a3acadf18cf49b2746001613d47c10ada
Denis Marmyshev <marmyshev.denis@mail.ru>
2024-02-13 21:44:43 +0200

    KS 8_2. Задача 2 к Уроку 8
         Заменил i на более понятное название ingredient.
         убрал лишние переменные и добавило опреатор return, который останавливает функцию в случае если условие истина и возвращает результат.

--- src/main/kotlin/lesson8/lesson8_task2.kt
+++ src/main/kotlin/lesson8/lesson8_task2.kt
@@ -6,15 +6,13 @@ fun main() {
     val recipe = arrayOf("картошка", "соль", "перец", "паприка", "куркума", "чеснок")
     println("Введите нужный ингридиент: ")
     val userIngredient = readln()
-    var check: Int = 0
 
     for (ingredient in recipe) {
-        if (userIngredient == ingredient) println("Ингредиент $ingredient в рецепте есть")
-        check++
+        if (userIngredient == ingredient) {
+            println("Ингредиент $ingredient в рецепте есть")
+            return
+        }
     }
-
-    if (check == 0) {
-        println("Такого ингредиента в рецепте нет")
-    }
+    println("Такого ингредиента в рецепте нет")
 
 }
