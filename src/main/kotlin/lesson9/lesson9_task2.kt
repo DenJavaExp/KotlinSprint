675600b084ddc2a09c4263b3452331b6f67f7c2c
Denis Marmyshev <marmyshev.denis@mail.ru>
2024-02-16 21:35:40 +0200

    KS 9_2. Задача 2 к Уроку 9
       Создай новый список ингредиентов, состоящий из 3 шт.
       Нужно расширить функционал программы – пусть у
       пользователя будет возможность добавить к базовым
       ингредиентам из списка еще один свой.
    
       - после создания базового списка, выведи его в консоль:
       "В рецепте есть базовые ингредиенты: [базовый_список_ингредиентов]";
       - затем сообщи о возможности добавить свой:
       "Желаете добавить еще?” и активировать ввод с консоли;
       - если пользователь ввел “нет” или что-то иное –
       заверши программу, если пользователь ввел “да” – запроси:
       "Какой ингредиент вы хотите добавить?";
       - добавь считанное значение в базовый список;
       - в конце выведи сообщение со списком всех элементов такого типа:
       "Теперь в рецепте есть следующие ингредиенты: [список_ингредиентов]".

---
+++ src/main/kotlin/lesson9/lesson9_task2.kt
@@ -0,0 +1,19 @@
+package lesson9
+
+fun main() {
+
+    val listIngridient = mutableListOf<String>("перец", "чеснок", "соль")
+    println("В рецепте есть базовые ингредиенты: $listIngridient")
+    println("Желаете добавить еще?")
+    val userDecision = readln()
+    if (userDecision == "да") {
+        println("Какой ингредиент вы хотите добавить?")
+        val userIngridient = readln()
+        listIngridient.add(userIngridient)
+        println("Теперь в рецепте есть следующие ингредиенты: $listIngridient")
+    } else {
+        return
+    }
+
+}
+
