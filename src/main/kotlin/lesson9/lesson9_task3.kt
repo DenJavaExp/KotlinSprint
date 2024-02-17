174836002f659ac6fa0e6b24cd90acac1bdd9b12
Denis Marmyshev <marmyshev.denis@mail.ru>
2024-02-17 19:01:30 +0200

    KS 9_3. Задача 3 к Уроку 9
       Напиши программу, которая будет рассчитывать сколько потребуется
       ингредиентов для разного количества порций блюда.
       Для примера возьмем рецепт омлета. Для одной порции нужно: яиц – 2шт.,
       молока – 50 мл, сливочного масла – 15 гр.
    
       – создай список с количеством ингредиентов для 1 блюда
       (где под индексами располагается количество для яиц/молока/масла соответственно);
       – запроси у пользователя количество порций;
       – модернизировать список таким образом, чтобы вывести в консоль
       сообщение с количеством нужных ингредиентов такого типа:
       “На [количество_порций] порций вам понадобится: Яиц – n,
       молока – n1, сливочного масла – n2]”.

---
+++ src/main/kotlin/lesson9/lesson9_task3.kt
@@ -0,0 +1,20 @@
+package lesson9
+
+fun main() {
+
+    val listIngridient = mutableListOf<Int>(2, 50, 15)
+    println("Какое количество порций омлета вы хотите приготовить? ")
+    val userPortions = readln().toInt()
+    val newListIngridient = listIngridient.map { it * userPortions }
+    println(
+        "На $userPortions порций вам понадобится: " +
+                "Яиц ${newListIngridient[0]}– " +
+                "молока – ${newListIngridient[1]}, " +
+                "сливочного масла – ${newListIngridient[2]}"
+    )
+
+}
+
+
+
+
