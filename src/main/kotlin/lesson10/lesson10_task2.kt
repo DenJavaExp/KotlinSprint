502abecf53a5aef2161e6ce4711009e7e5541077
Denis Marmyshev <marmyshev.denis@mail.ru>
2024-02-29 10:23:09 +0200

    KS 10_1. Задача 1 к Уроку 10
             Переименновал функци в более понятные названия.
             Удалил из функции comparesPoints лишнее. Добавил пустую строкуи отделил обьявление переменных от функций.
    
             Но, оставил size, так как у CharArray нет функции .length, возможно я не правильно понял где именно стоит применить эту функцию.

--- src/main/kotlin/lesson10/lesson10_task2.kt
+++ src/main/kotlin/lesson10/lesson10_task2.kt
@@ -3,20 +3,24 @@ package lesson10
 fun main() {
     val userLogin = readln()
     val userPassword = readln()
-    check(userPassword, userLogin)
+    val charArreyLogin: CharArray = toCharacterArray(userLogin)
+    val charArreyPassword: CharArray = toCharacterArray(userPassword)
+
+    calculateLength(charArreyLogin, charArreyPassword)
 }
 
-fun check(arreyPassword: String, arreyLogin: String) {
-    val chArreyLogin: CharArray = toCharacterArray(arreyPassword)
-    val chArreyPassword: CharArray = toCharacterArray(arreyLogin)
-    if (chArreyPassword.size < 4 && chArreyLogin.size < 4) {
+const val MIN_LENGTH_LOGIN_PASSWORD = 4
+
+
+fun calculateLength(charArreyLogin: CharArray, charArreyPassword: CharArray) {
+    if (charArreyPassword.size > MIN_LENGTH_LOGIN_PASSWORD && charArreyLogin.size > MIN_LENGTH_LOGIN_PASSWORD) {
         println("Логин или пароль недостаточно длинные")
     } else {
         println("Welcome")
     }
 }
 
-fun toCharacterArray   (str:String):CharArray{
+fun toCharacterArray(str: String): CharArray {
     return str.toCharArray()
 }
 
