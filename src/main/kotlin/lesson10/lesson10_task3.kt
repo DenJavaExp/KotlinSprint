e75ced8872144ee1bc975ceba0a97d27bda1e2b5
Denis Marmyshev <marmyshev.denis@mail.ru>
2024-03-01 12:52:50 +0200

    KS 10_3. Задача 3 к Уроку 10
          -Создал два диапозона с символами и числами.
          -Циклом в зависимости от четности добавляю в новый массив элементы из диапозонв, таким образом генерирую пароль.

--- src/main/kotlin/lesson10/lesson10_task3.kt
+++ src/main/kotlin/lesson10/lesson10_task3.kt
@@ -1,21 +1,32 @@
 package lesson10
 
 fun main() {
-    val asciiRange: List<Char> =
-        (START_SYMBOL..END_SYMBOL).map { it.toChar() } //+ ' ' не знаю почему, но пробел все равно включается в диапозон, хотя его там нет.
+    val asciiRangeSymbol: List<Char> =
+        (START_SYMBOL..END_SYMBOL).map { it.toChar() }
+    val asciiRangeNumber: List<Char> =
+        (START_NUMBER..END_NUMBER).map { it.toChar() }
+
     println("Какую длуну пароля вы хотите?")
     val lengthPassword = readln().toInt()
-//    println(asciiRange.joinToString("")) // ля просмротра дипозона.
-    println(generatePassword(lengthPassword, asciiRange.toString()))
+    println(generatePassword(lengthPassword, asciiRangeSymbol, asciiRangeNumber))
 }
 
-const val START_SYMBOL = 33
-const val END_SYMBOL = 57
+const val START_SYMBOL = 32
+const val END_SYMBOL = 47
+const val START_NUMBER = 48
+const val END_NUMBER = 57
 
-fun generatePassword(lengthPassword: Int, asciiRange: String): String {
-    val generatePassword = (asciiRange.indices).shuffled().take(lengthPassword)
-    val randomChars = generatePassword.map { asciiRange[it] }
+fun generatePassword(lengthPassword: Int, asciiRangeSymbol: List<Char>, asciiRangeNumber: List<Char>): String {
+    val generatePassword = arrayOfNulls<String>(lengthPassword)
 
-    return randomChars.joinToString("")
+    for (i in generatePassword.indices) {
+
+        if (i % 2 == 0) {
+            generatePassword[i] = asciiRangeSymbol.random().toString()
+        } else {
+            generatePassword[i] = asciiRangeNumber.random().toString()
+        }
+    }
+    return generatePassword.joinToString("")
 }
 
