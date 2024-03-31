46a2a5cde74e6cdd57525bcfae2ccd2d91cf11b7
Denis Marmyshev <marmyshev.denis@mail.ru>
2024-03-31 21:28:08 +0200

    KS 13_3. Задача 3 к Уроку 13
     Скопируй класс из предыдущей задачи и измени его название для избегания конфликтов.
    
     Для того, чтобы взаимодействовать со всеми записями телефонной книги,
     как с одним целым, их нужно объединить в список.
    
     - создай и инициализируй (одновременно) список с 5 объектами класса-контакта;
     - 2 контакта должны принимать null для поля компании;
     - 1 контакт должен иметь название компании “null”;
     - выведи список всех существующих компаний, которые есть в телефонной книге.

---
+++ src/main/kotlin/lesson13/lesson13_task3.kt
@@ -0,0 +1,40 @@
+class PhoneBook(
+    val name: String,
+    val telephoneNumber: Long,
+    val companyName: String?,
+)
+
+fun main() {
+    val listPhoneBook =
+        listOf(
+            PhoneBook(
+                name = "Denis",
+                telephoneNumber = 88008888555L,
+                companyName = null,
+            ),
+            PhoneBook(
+                name = "Ruslan",
+                telephoneNumber = 88008888444L,
+                companyName = null,
+            ),
+            PhoneBook(
+                name = "Andrey",
+                telephoneNumber = 88008888333L,
+                companyName = "null",
+            ),
+            PhoneBook(
+                name = "Vladimir",
+                telephoneNumber = 88008888111L,
+                companyName = "NovoTek",
+            ),
+            PhoneBook(
+                name = "Ivan",
+                telephoneNumber = 88008888888L,
+                companyName = "NorNickel",
+            ),
+        )
+
+    for (person in listPhoneBook) {
+        println(person.companyName)
+    }
+}
