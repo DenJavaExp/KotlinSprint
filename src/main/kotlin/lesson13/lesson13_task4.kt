91b044665b4885f1250c9f6fe7eb9acb3b2e2153
Denis Marmyshev <marmyshev.denis@mail.ru>
2024-04-09 20:01:58 +0200

    KS 13_4. Задача 4 к Уроку 13
     Скопируй класс из предыдущей задачи и измени его название для избегания конфликтов.
    
     Программа должна заполнять контакты в телефонную книгу (отдельный список объектов).
    
     - если пользователь не ввел номер телефона, то нужно уведомить об этом
     пользователя и такая запись не должна добавляться;
     - для приведения введенного номера к числу используй toLongOrNull();
     - если пользователь не заполнил компанию, то в объект записывать null.
    
     Выведи созданные объекты в консоль методом класса.

--- src/main/kotlin/lesson13/lesson13_task4.kt
+++ src/main/kotlin/lesson13/lesson13_task4.kt
@@ -1,48 +1,47 @@
-class BookPhone(
-    var name: String?,
+class Contact(
+    var name: String,
     var telephoneNumber: Long?,
     var companyName: String?,
-) {
-    fun addPersonList(listPhoneBook: MutableList<BookPhone>) {
-        while (true) {
-            println("Хотите добавить нового клиента?")
-            val userResponse = readln()
-            if (userResponse == "да") {
-                println("Введите имя клиента:")
-                name = readln()
-                var controlTelephon = true
-                while (controlTelephon) {
-                    println("Введите номер клиента:")
-                    telephoneNumber = readln().toLongOrNull()
-                    if (telephoneNumber != null) {
-                        controlTelephon = false
-                    } else {
-                        println("Вы не ввели номер телефона!")
-                    }
-                }
+)
 
-                println("Введите название компании:")
-                companyName = readln()
-                val person = BookPhone(name = name, telephoneNumber = telephoneNumber, companyName = companyName)
-                listPhoneBook.add(person)
-            } else {
-                break
+fun addPersonList(person: Contact): Contact {
+    while (true) {
+        println("Хотите добавить нового клиента?")
+        val userResponse = readln()
+        if (userResponse == "да") {
+            println("Введите имя клиента:")
+            person.name = readln()
+            var controlTelephon = true
+            while (controlTelephon) {
+                println("Введите номер клиента:")
+                person.telephoneNumber = readln().toLongOrNull()
+                if (person.telephoneNumber != null) {
+                    controlTelephon = false
+                } else {
+                    println("Вы не ввели номер телефона!")
+                }
             }
+
+            println("Введите название компании:")
+            person.companyName = readln()
+        } else {
+            break
         }
     }
+    return person
+}
 
-    fun printPersonList(listPhoneBook: MutableList<BookPhone>) {
-        for (person in listPhoneBook) {
-            println("Имя клиента: ${person.name}")
-            println("Тлефон клиента: ${person.telephoneNumber}")
-            println("Название компании: ${person.companyName}")
-        }
+fun printPersonList(listPhoneBook: MutableList<Contact>) {
+    for (person in listPhoneBook) {
+        println("Имя клиента: ${person.name}")
+        println("Тлефон клиента: ${person.telephoneNumber}")
+        println("Название компании: ${person.companyName}")
     }
 }
 
 fun main() {
-    val listPhoneBook = mutableListOf<BookPhone>()
-    val person = BookPhone(name = "", telephoneNumber = 0L, companyName = "")
-    person.addPersonList(listPhoneBook)
-    person.printPersonList(listPhoneBook)
+    val listPhoneBook = mutableListOf<Contact>()
+    val person = Contact(name = "", telephoneNumber = 0L, companyName = "")
+    listPhoneBook.add(addPersonList(person))
+    printPersonList(listPhoneBook)
 }
