38e9839bce5d40ade915206ea83a038095cc8cde
Denis Marmyshev <marmyshev.denis@mail.ru>
2024-04-15 15:16:05 +0200

    KS 14_1. Задача 1 к Уроку 14
     Закинул все в один файл.

--- src/main/kotlin/lesson14_task1/Main.kt
+++ src/main/kotlin/lesson14_task1/Main.kt
@@ -1,3 +1,40 @@
+open class LinerShip(
+    val name: String,
+    val speed: Int,
+    var loadCapacity: Int,
+    val passengerCapacity: Int = 1500,
+) {
+    fun boardingPassengers() {
+        println("$name обьявил посадку пассажиров с вместимостью $passengerCapacity персон")
+    }
+
+    fun sailAway() {
+        println("$name начал движение и набрал максимаьную скорость $speed узлов!")
+    }
+
+    fun moor() {
+        println("$name снизил ход с максимальной скорости $speed узлов и начал пришвартовываться")
+    }
+}
+
+class CargoShip(
+    name: String,
+    speed: Int,
+) : LinerShip(name, speed, 1000, 0) {
+    fun startLoadingIntoHold() {
+        println("$name начата погрузка в трюмы обьемом $loadCapacity тонн.")
+    }
+}
+
+class Icebreaker(
+    name: String,
+    speed: Int,
+) : LinerShip(name, speed, 500, 0) {
+    fun startCrackingIce() {
+        println("$name включил режим колки льда.")
+    }
+}
+
 fun main() {
     val cargoShip =
         CargoShip("Svetlov", 200)
