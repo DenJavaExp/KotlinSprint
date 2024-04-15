98d2efbf3db55ee5f7c027d200d69a3c09622dab
Denis Marmyshev <marmyshev.denis@mail.ru>
2024-04-15 15:17:39 +0200

    Delete src/main/kotlin/lesson14_task1/Icebreaker.kt

--- src/main/kotlin/lesson14_task1/Icebreaker.kt
+++
@@ -1,8 +0,0 @@
-class Icebreaker(
-    name: String,
-    speed: Int,
-) : LinerShip(name, speed, 500, 0) {
-    fun startCrackingIce() {
-        println("$name включил режим колки льда.")
-    }
-}
