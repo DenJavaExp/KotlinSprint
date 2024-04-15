8c3614e22c28ce5519a5d45119d28d021ce19125
Denis Marmyshev <marmyshev.denis@mail.ru>
2024-04-15 15:17:26 +0200

    Delete src/main/kotlin/lesson14_task1/CargoShip.kt

--- src/main/kotlin/lesson14_task1/CargoShip.kt
+++
@@ -1,8 +0,0 @@
-class CargoShip(
-    name: String,
-    speed: Int,
-) : LinerShip(name, speed, 1000, 0) {
-    fun startLoadingIntoHold() {
-        println("$name начата погрузка в трюмы обьемом $loadCapacity тонн.")
-    }
-}
