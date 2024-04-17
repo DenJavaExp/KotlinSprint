35ad8e28e03b5241ea0721ac6790b7c2088d07b5
Denis Marmyshev <marmyshev.denis@mail.ru>
2024-04-17 17:58:17 +0200

    KS 14_3. Задача 3 к Уроку 14
    Добавил константы и удалил измняемый список.

--- src/main/kotlin/lesson14/lesson14_task3.kt
+++ src/main/kotlin/lesson14/lesson14_task3.kt
@@ -1,5 +1,7 @@
 import kotlin.math.roundToInt
 
+const val COLOR_BLACK = "black"
+const val COLOR_WITHE = "white"
 const val PI = 3.14
 
 abstract class Figure {
@@ -38,31 +40,31 @@ class Rectangle(
 }
 
 fun main() {
-    val circle = Circle(10.0, "black") // 68
+    val circle = Circle(10.0, COLOR_BLACK) // 68
     circle.calculateArea()
     circle.calculatePerimeter()
 
-    val circle1 = Circle(15.4, "white")
+    val circle1 = Circle(15.4, COLOR_WITHE)
     circle1.calculateArea()
     circle1.calculatePerimeter()
 
-    val circle2 = Circle(2.4, "white")
+    val circle2 = Circle(2.4, COLOR_WITHE)
     circle2.calculateArea()
     circle2.calculatePerimeter()
 
-    val rectangle = Rectangle(5.0, 7.0, "white")
+    val rectangle = Rectangle(5.0, 7.0, COLOR_WITHE)
     rectangle.calculateArea()
     rectangle.calculatePerimeter()
 
-    val rectangle1 = Rectangle(5.5, 10.2, "black")
+    val rectangle1 = Rectangle(5.5, 10.2, COLOR_BLACK)
     rectangle1.calculateArea()
     rectangle1.calculatePerimeter()
 
-    val rectangle2 = Rectangle(2.1, 5.3, "white")
+    val rectangle2 = Rectangle(2.1, 5.3, COLOR_WITHE)
     rectangle2.calculateArea()
     rectangle2.calculatePerimeter()
 
-    val figuresList = mutableListOf(circle, circle1, circle2, rectangle, rectangle1, rectangle2)
+    val figuresList = listOf(circle, circle1, circle2, rectangle, rectangle1, rectangle2)
     var sumArea = 0.0
     var sumRectangle = 0.0
     for (figure in figuresList) {
