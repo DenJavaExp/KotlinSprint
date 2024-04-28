5e49d190242d110696cd141848831fe6981b4134
Denis Marmyshev <marmyshev.denis@mail.ru>
2024-04-28 20:50:57 +0200

    KS 16_2. Задача 2 к Уроку 16
     Исправил опечатку.
     Использовал стандартную функцию возведения в степень

--- src/main/kotlin/lesson16/lesson16_task2.kt
+++ src/main/kotlin/lesson16/lesson16_task2.kt
@@ -1,14 +1,17 @@
+import kotlin.math.pow
+import kotlin.math.roundToInt
+
 private const val P = 3.14
 
 class Ring(
     private val radius: Double,
 ) {
     fun calculateLengthCircle() {
-        println("Длина окружности равна: ${2 * P * 1}")
+        println("Длина окружности равна: ${((2 * P * radius) * 100.0).roundToInt() / 100.0}")
     }
 
     fun calculateAreaCircle() {
-        println("Плащадь круга равна: ${P * (radius * radius)}")
+        println("Плащадь круга равна: ${(P * (radius.pow(2.0)) * 100.0).roundToInt() / 100.0}")
     }
 }
 
