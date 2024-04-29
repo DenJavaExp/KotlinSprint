1557b3d5c695a0dc592b34d479b01e22113638b1
Denis Marmyshev <marmyshev.denis@mail.ru>
2024-04-29 17:29:11 +0200

    KS 16_4. Задача 4 к Уроку 16
        Создай класс, описывающий заказ в интернет-магазине.
        В заказе должны быть номер заказа и статус готовности.
        Номер заказа должен быть недоступен для изменения.
    
        Создай метод, который меняет статус заказа.
        Поменять статус заказа можно только отправив заявку менеджеру.
        Имитируй это действие отдельной публичной функцией.
        Она будет принимать новый статус и обращаться к внутреннему методу класса для изменения статуса.

---
+++ src/main/kotlin/lesson16/lesson16_task4.kt
@@ -0,0 +1,22 @@
+class Order(
+    private val numberOrder: Int,
+    private var statusOrder: Boolean,
+) {
+    fun setStatesOrder(newStatusOrder: Boolean) {
+        statusOrder = newStatusOrder
+    }
+
+    fun changeOrderStatus(newStatusOrder: Boolean) {
+        setStatesOrder(newStatusOrder)
+        if (newStatusOrder == true) {
+            println("Статус заказа изменен на ГОТОВ")
+        } else {
+            println("Статус заказа В ОБРАБОТКЕ")
+        }
+    }
+}
+
+fun main() {
+    val order = Order(1112332, false)
+    order.changeOrderStatus(true)
+}
