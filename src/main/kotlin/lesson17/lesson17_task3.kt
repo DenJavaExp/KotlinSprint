bbe1a417fdae7a8a254d6c43332de0f11841a09e
Denis Marmyshev <marmyshev.denis@mail.ru>
2024-05-06 22:41:05 +0200

    KS 17_3. Задача 3 к Уроку 17
     -Добавил поле name;
     -Использовал в рпботе get()

--- src/main/kotlin/lesson17/lesson17_task3.kt
+++ src/main/kotlin/lesson17/lesson17_task3.kt
@@ -1,19 +1,16 @@
-class Folder() {
-    private var sicret = true
-    private val filesNumber = 200
-
-    fun getFilesNumber(): Int {
-        if (sicret) {
-            println("“скрытая папка” и количество файлов – 0")
-        } else {
-            println(!sicret)
-            println("количесто файлов в папке $filesNumber")
+class Folder(name: String, private val filesNumber: Int, private var sicret: Boolean = true) {
+    val name = name
+        get() {
+            if (this.sicret) {
+                println("“скрытая папка” и количество файлов – 0")
+            } else {
+                println("количесто файлов в папке $filesNumber")
+            }
+            return field
         }
-        return filesNumber
-    }
 }
 
 fun main() {
-    val folder = Folder()
-    println(folder.getFilesNumber())
+    val folder = Folder("Fails", 200)
+    println(folder.name)
 }
