dc4278d4610ce29e46665dfcf4161faf54e1bd3b
Denis Marmyshev <marmyshev.denis@mail.ru>
2024-05-12 14:38:51 +0200

    KS 18_5. Задача 5 к Уроку 18
     В приложении есть экран. Мы можем рисовать в нем разные объекты, указывая координаты (x, y) и сам объект.
     Объектами выступают круг, квадрат и точка. Координаты могут быть как Int, так и Float.
    
     Реализуй классы точки, квадрата и круга, а также класс Screen со всеми перегруженными методами draw(...).

---
+++ src/main/kotlin/lesson18/lesson18_task5.kt
@@ -0,0 +1,34 @@
+open class Figura(
+    val name: String,
+)
+
+class Ring(name: String, val coordinateX: Float, val coordinateY: Float, val radius: Float) : Figura(name)
+
+class Square(name: String, val coordinateX: Float, val coordinateY: Float, val sideLength: Float) : Figura(name)
+
+class Dot(name: String, val coordinateX: Int, val coordinateY: Int) : Figura(name)
+
+class Screen {
+    fun draw(ring: Ring) {
+        println("Фигура ${ring.name} нарисованна в координатах по оси X - ${ring.coordinateX} и по оси Y - ${ring.coordinateY}")
+    }
+
+    fun draw(square: Square) {
+        println("Фигура ${square.name} нарисованна в координатах по оси X - ${square.coordinateX} и по оси Y - ${square.coordinateY}")
+    }
+
+    fun draw(dot: Dot) {
+        println("Фигура ${dot.name} нарисованна в координатах по оси X - ${dot.coordinateX} и по оси Y - ${dot.coordinateY}")
+    }
+}
+
+fun main() {
+    val ring = Ring("Круг", 4.4f, 5.5f, 3.1f)
+    val square = Square("Квадрат", 6.1f, 7.3f, 5.0f)
+    val dot = Dot("Точка", 10, 11)
+    val screen = Screen()
+
+    screen.draw(ring)
+    screen.draw(square)
+    screen.draw(dot)
+}
