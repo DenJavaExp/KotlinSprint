6d5c8dbf9380a0df9b73716841c1a2d4e57bee14
Denis Marmyshev <marmyshev.denis@mail.ru>
2024-05-23 21:11:56 +0200

    KS 19_3. Задача 3 к Уроку 19
     В игре есть космический корабль. Он может взлетать, приземляться и отстреливаться от астероида.
    
     Задача описать класс и его методы. Требуемая реализация методов нам пока неизвестна.
     Один из методов должен содержать явную пометку-напоминание о том, что здесь нужна информация
     о дополнительной логике.
     Другой метод должен вызывать падение программы с ошибкой NotImplementedError.

---
+++ src/main/kotlin/lesson19/lesson19_task3.kt
@@ -0,0 +1,19 @@
+class CosmoShip() {
+    fun takeoff() {
+        // TODO(не забудь прописать реализацию)
+    }
+
+    fun boarding() {
+        TODO("не забудь прописать реализацию")
+    }
+
+    fun shootBack() {
+    }
+}
+
+fun main() {
+    val cosmoShip = CosmoShip()
+
+    cosmoShip.takeoff()
+    cosmoShip.boarding()
+}
