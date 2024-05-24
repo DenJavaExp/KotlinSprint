69c0101079e1b33155bcc7994ac4e99a05639ff2
Denis Marmyshev <marmyshev.denis@mail.ru>
2024-05-24 21:14:15 +0200

    KS 19_4. Задача 4 к Уроку 19
     Внес исправления согласно комментарию

--- src/main/kotlin/lesson19/lesson19_task4.kt
+++ src/main/kotlin/lesson19/lesson19_task4.kt
@@ -4,24 +4,24 @@ enum class Cartridges(val unitDamage: Int) {
     RED(20),
 }
 
-class Tank(private var unitDamage: Int = 0) {
-    fun loadNewCartridges(unitDamage: Int) {
-        this.unitDamage = unitDamage
+class Tank(private var unitDamage: Cartridges? = null) {
+    fun loadNewCartridges(cartridge: Cartridges) {
+        this.unitDamage = cartridge
         println("Танк заряжен потронами с уроном $unitDamage")
     }
 
     fun shot() {
-        return println("Танк выстрелил и нанес урон $unitDamage")
+        return println("Танк выстрелил и нанес урон ${unitDamage?.unitDamage ?: 0}")
     }
 }
 
 fun main() {
     val tank1 = Tank()
 
-    tank1.loadNewCartridges(Cartridges.BLUE.unitDamage)
+    tank1.loadNewCartridges(Cartridges.BLUE)
     tank1.shot()
-    tank1.loadNewCartridges(Cartridges.GREEN.unitDamage)
+    tank1.loadNewCartridges(Cartridges.GREEN)
     tank1.shot()
-    tank1.loadNewCartridges(Cartridges.RED.unitDamage)
+    tank1.loadNewCartridges(Cartridges.RED)
     tank1.shot()
 }
