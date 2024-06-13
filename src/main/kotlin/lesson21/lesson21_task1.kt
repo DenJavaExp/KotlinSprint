c9e25a53f1cb7fc5fdce100356dd7caf8b6a2154
My Name <marmyshev.denis@mail.ru>
2024-06-13 21:19:05 +0200

    KS 21_1. Задача 1 к Уроку 21
    Добавил this.count { it in vowel } и удалил лишнее

--- src/main/kotlin/lesson21/lesson21_task1.kt
+++ src/main/kotlin/lesson21/lesson21_task1.kt
@@ -1,23 +1,16 @@
 fun main() {
-    fun String.vowelCount(word: String) {
-        var wordList = listOf<Char>()
+    fun String.vowelCount() {
         val vowel =
             setOf(
                 'a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U',
                 'а', 'е', 'ё', 'и', 'о', 'у', 'ы', 'э', 'ю', 'я',
                 'А', 'Е', 'Ё', 'И', 'О', 'У', 'Ы', 'Э', 'Ю', 'Я',
             )
-        wordList = word.toList()
-        var countVowel = 0
-        for (i in wordList) {
-            if (i in vowel) {
-                countVowel++
-            }
-        }
-        println("В слове $word глассных букв $countVowel")
+
+        println(this.count { it in vowel })
     }
 
     println("Введите слово:")
     val word = readln()
-    word.vowelCount(word)
+    word.vowelCount()
 }
