58516e5339c68fc4f1fd135cf6665ae08b010a92
My Name <marmyshev.denis@mail.ru>
2024-06-14 21:30:54 +0200

    KS 21_1. Задача 1 к Уроку 21
    Добавил this и удалил предачу параметра.

--- src/main/kotlin/lesson21/lesson21_task2.kt
+++ src/main/kotlin/lesson21/lesson21_task2.kt
@@ -1,9 +1,9 @@
 fun main() {
-    fun List<Int>.evenNumbersSum(listNum: List<Int>) {
-        val result = listNum.filter { it % 2 == 0 }
+    fun List<Int>.evenNumbersSum() {
+        val result = this.filter { it % 2 == 0 }
         println(result.sum())
     }
 
     val listNum = listOf<Int>(4, 22, 1, 37, 12, 11)
-    listNum.evenNumbersSum(listNum)
+    listNum.evenNumbersSum()
 }
