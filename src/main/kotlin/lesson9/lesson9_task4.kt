6a631d517f11104fa88a7f81ef61d77b9f602d55
My Name <marmyshev.denis@mail.ru>
2024-06-21 22:08:44 +0200

    KS 9_4. Задача 4 к Уроку 9
    -Удалил лишние переменные

--- src/main/kotlin/lesson9/lesson9_task4.kt
+++ src/main/kotlin/lesson9/lesson9_task4.kt
@@ -1,12 +1,7 @@
 package lesson9
 
 fun main() {
-
     println("Напишите 5 ингридиентов через запяту и пробел:")
-    val ingridientList = readln()
-    val delim = ", "
-    val newIngridientList = ingridientList.split(delim).toMutableList()
+    val newIngridientList = readln().split(", ").toMutableList()
     println(newIngridientList.sorted())
-
 }
-
