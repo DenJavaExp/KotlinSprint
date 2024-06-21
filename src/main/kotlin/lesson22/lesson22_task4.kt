b8c33415537613c827480a0e834ebb1bf460e101
My Name <marmyshev.denis@mail.ru>
2024-06-21 17:52:55 +0200

    KS 22_4. Задача 4 к Уроку 22
    -Добавил функцию copy, копирую свойства объекта status изменяя свойства loadingData, isLoding.

--- src/main/kotlin/lesson22/lesson22_task4.kt
+++ src/main/kotlin/lesson22/lesson22_task4.kt
@@ -2,15 +2,16 @@ class MainScreenViewModel(mainScreenState: MainScreenState) {
     data class MainScreenState(val loadingData: String = "", val isLoding: Boolean = false)
 
     fun loadData() {
+        val status = MainScreenState()
         for (state in 0..3) {
             if (state == 1) {
-                val status1 = MainScreenState(loadingData = "Ожидаем загрузку данных")
+                val status1 = status.copy(loadingData = "Ожидаем загрузку данных")
                 println(status1)
             } else if (state == 2) {
-                val status2 = MainScreenState(loadingData = "Начата загрузка данных", isLoding = true)
+                val status2 = status.copy(loadingData = "Идёт загрузка данных", isLoding = true)
                 println(status2)
             } else if (state == 3) {
-                val status3 = MainScreenState(loadingData = "Загрузка данных окончена")
+                val status3 = status.copy(loadingData = "Загрузка данных окончена")
                 println(status3)
             }
         }
