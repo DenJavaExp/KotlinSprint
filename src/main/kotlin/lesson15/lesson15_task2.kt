4992d05ade82df39798d62c7753178d3a043ef81
My Name <marmyshev.denis@mail.ru>
2024-06-29 15:03:29 +0200

    KS 15_2. Задача 2 к Уроку 15
    -удалил лишнее и добавил функцию, которая принимает обьект и в зависимости от типа выводит свойства (температура или осадки)

--- src/main/kotlin/lesson15/lesson15_task2.kt
+++ src/main/kotlin/lesson15/lesson15_task2.kt
@@ -1,24 +1,28 @@
 abstract class WeatherStationStats
 
-class Temperature(val temperature: Int) : WeatherStationStats()
+class Temperature(val dateTemperature: Int) : WeatherStationStats()
 
-class PrecipitationAmount(val precipitationAmount: String) : WeatherStationStats()
+class PrecipitationAmount(val datePrecipitationAmount: Int) : WeatherStationStats()
 
 class WeatherServer() {
-    fun sendWeatherMessage(temperature: Int) {
-        println("Температура $temperature градусов Цельсия")
-    }
+    fun sendWeatherMessage(date: WeatherStationStats) {
+        when (date) {
+            is Temperature -> {
+                println("Температура воздуха состовляет ${date.dateTemperature} градусов Цельсия")
+            }
 
-    fun sendWeatherMessage(precipitationAmount: String) {
-        println("Ожидается $precipitationAmount")
+            is PrecipitationAmount -> {
+                println("Уровень осадков сегодня состовляет ${date.datePrecipitationAmount} см от месячной нормы")
+            }
+        }
     }
 }
 
 fun main() {
-    val temperature = Temperature(25)
-    val precipitationAmount = PrecipitationAmount("Дождь")
     val weatherServer = WeatherServer()
+    val temperature = Temperature(25)
+    val precipitationAmount = PrecipitationAmount(23)
 
-    weatherServer.sendWeatherMessage(temperature.temperature)
-    weatherServer.sendWeatherMessage(precipitationAmount.precipitationAmount)
+    weatherServer.sendWeatherMessage(temperature)
+    weatherServer.sendWeatherMessage(precipitationAmount)
 }
